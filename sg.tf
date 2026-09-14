3e989a5951cc5344d5e269d86af8344e352ce991
ParvaKaria <parva.karia@prominentpixel.com>
2026-09-14 18:24:32 +0530

    Create empty security group in sg.tf

---
+++ sg.tf
@@ -0,0 +1,7 @@
+resource "aws_security_group" "empty_sg" {
+  name        = "empty-security-group"
+  description = "Security group with no inbound or outbound rules"
+
+  # No ingress block
+  # No egress block
+}
